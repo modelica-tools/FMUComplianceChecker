09bc8b943e350dfae14d94c936b8c41d3617bbb9
Iakov Nakhimovski <iakov.nakhimovski@modelon.com>
2012-07-06 10:03:11 +0000

    Creating tag 1.0

---
+++ tags/1.0/README-FMUChecker.txt
@@ -0,0 +1,69 @@
+File: README-FMUChecker.txt
+Background information for FMI Compliance Checher (FMUChecker) application.
+
+For build instructions see: BUILD-FMUChecker.txt
+Licence information is provided in: LICENCE-FMUChecker.txt
+Acknowledgements for used software: ACKNOWLEDGEMENTS-FMUChecker.txt
+
+The FMI Compliance Checker is intended for
+validation of FMU 1.0 (and later 2.0) compliance to the standard specification
+as published at <http://functional-mockup-interface.org>
+
+The basic features include:
+ - automatic unzipping into a temporary directory;
+ - checking of XML model description
+	- for syntax errors;
+	- for correct order of elements and correct cardinality or relations;
+	- for correct cross-references;
+	- for semantic consistency;
+ - validation of binary FMUs compiled for "standard32" platform for 
+	Windows (.dll), Linux (.so) and Mac OS (.dylib).
+	- loading of the binary module;
+	- checking whether all required functions are available
+	- test whether the FMU can be simulated with explicit (forward) 
+	Euler method
+		- fixed step size is used;
+		- no iterations for exact location of state events;
+	- log computed solution to csv result file (comma separated values;
+	first row variable names, first column time).
+ - validation log messages are written to stderr. Can be redirected to file.
+ 
+Usage: fmuCheck.<platform> [options] <model.fmu>
+
+Options:
+
+-c <separator>   Separator character to be used in CSV output. Default is ';'.
+
+-e <filename>    Error log file name. Default is to use standard error.
+
+-h <stepSize>    Step size to use in forward Euler. Takes preference over '-n'.
+
+-l <log level>   Log level: 0 - no logging, 1 - fatal errors only,
+                 2 - errors, 3 - warnings, 4 - info, 5 - verbose, 6 - debug.
+
+-n <num_steps>   Number of steps in forward Euler until time end.
+                 Default is 100 steps simulation between start and stop time.
+
+-o <filename>    Simulation result output file name. Default is to use standard output.
+
+-s <stopTime>    Simulation stop time, default is to use information from
+                'DefaultExperiment' as specified in the model description XML.
+
+-t <tmp-dir>     Temporary dir to use for unpacking the FMU.
+                 Default is to use system-wide directory, e.g., C:\Temp.
+
+-x               Check XML and stop. Default is to load the DLL and simulate
+                 after this.
+
+Command line examples:
+
+fmuCheck.win32 model.fmu 
+        The checker on win32 platform will process "model.fmu"  with default options.
+                
+fmuCheck.win64 -e log.txt -o result.csv -c , -s 2 -h 1e-3 -l 5 -t . model.fmu 
+        The checker on win64 platform will process "model.fmu". The log 
+        messages will be saved in log.txt, simulation output in 
+        result.csv and comma will be used for field separation in the CSV
+        file. The checker will simulate the FMU until 2 seconds with 
+        time step 1e-3 seconds. Verbose messages will be generated.
+        Temporary files will be created in the current directory.
---
+++ tags/1.0/include/fmuChecker.h
@@ -0,0 +1,139 @@
+/*
+    Copyright (C) 2012 Modelon AB <http://www.modelon.com>
+
+	You should have received a copy of the LICENCE-FMUChecker.txt
+    along with this program. If not, contact Modelon AB.
+*/
+/**
+	\file fmuChecker.h
+	Header file for fmuChecker application.
+*/
+
+#ifndef FMUCHECKER_H_
+#define FMUCHECKER_H_
+
+#include <stdio.h>
+#include <JM/jm_portability.h>
+#include <fmilib.h>
+
+/** string constant used for logging. */
+extern const char* fmu_checker_module;
+
+/** Common exit routine */
+void do_exit(int code);
+
+/** Global variable used for checking alloc/free consistency */
+extern int allocated_mem_blocks;
+
+/** malloc proxy */
+void* check_malloc(size_t size);
+
+/** calloc proxy */
+void* check_calloc(size_t nobj, size_t size);
+
+/** free proxy */
+void  check_free(void* obj);
+
+/** Print information on command line options */
+void print_usage() ;
+
+#define MAX_URL_LENGTH 10000
+
+/**  Checker data structure is used to pass information between different routines */
+typedef struct fmu_check_data_t {
+	/** FMU file */
+	const char* FMUPath;	
+	/** Temporary directory with unique name where FMU is unpacked */
+	char* tmpPath;
+
+	/** Same as above with file:// prefix. Used for CS FMUs */
+	char fmuLocation[MAX_URL_LENGTH];
+
+	/** Directory to be used for temporary files. Either user specified or system-wide*/
+	const char* temp_dir;
+
+	/** Counter for the warnings */
+	unsigned int num_warnings;
+
+	/** counter for the non-fatal errors */
+	unsigned int num_errors;
+
+	/** counter for the non-fatal errors */
+	unsigned int num_fatal;
+
+	/** counter for the non-info messages from FMU */
+	unsigned int num_fmu_messages;
+
+	/** FMIL callbacks*/
+	jm_callbacks callbacks;
+	/** FMIL context */
+	fmi_import_context_t* context;
+
+	/** Model information */
+	const char* modelIdentifier;
+	const char* modelName;
+	const char*  GUID;
+	const char* instanceName;
+
+	/** Simulation stop time */
+	double stopTime;
+	/** Step size for the simulation*/
+	double stepSize;
+	/** Number of steps to take */
+	size_t numSteps;
+	/** separator character to use */
+	char CSV_separator;
+	/** Name of the output file (NULL is stdout)*/
+	char* output_file_name;
+	/** Output file stream */
+	FILE* out_file;
+	/** Name of the log file (NULL is stderr)*/
+	char* log_file_name;
+	/** Log file stream */
+	FILE* log_file;
+
+	/** Should simulation be done (or only XML checking) */
+	int do_simulate_flg;
+
+	/** FMI standard version of the FMU */
+	fmi_version_enu_t version;
+
+	/** FMI1 main struct */
+	fmi1_import_t* fmu1;
+	/** Kind of the FMI */
+	fmi1_fmu_kind_enu_t fmu1_kind;
+	/** model variables */
+	fmi1_import_variable_list_t* vl;
+} fmu_check_data_t;
+
+
+/** Global pointer is necessary to support FMI 1.0 logging */
+extern fmu_check_data_t* cdata_global_ptr;
+
+/** parse command line options and set fields in cdata accordingly */
+void parse_options(int argc, char *argv[], fmu_check_data_t* cdata);
+
+/** Init checker data with defaults */ 
+void init_fmu_check_data(fmu_check_data_t* cdata);
+
+/** Release allocated resources */ 
+void clear_fmu_check_data(fmu_check_data_t* cdata, int close_log);
+
+/** Logger function for FMI library */
+void checker_logger(jm_callbacks* c, jm_string module, jm_log_level_enu_t log_level, jm_string message);
+
+/** Check an FMI 1.0 FMU */
+jm_status_enu_t fmi1_check(fmu_check_data_t* cdata);
+
+/** Simulate an FMI 1.0 ME FMU */
+jm_status_enu_t fmi1_me_simulate(fmu_check_data_t* cdata);
+
+/** Simulate an FMI 1.0 CS FMU */
+jm_status_enu_t fmi1_cs_simulate(fmu_check_data_t* cdata);
+
+jm_status_enu_t fmi1_write_csv_header(fmu_check_data_t* cdata);
+
+jm_status_enu_t fmi1_write_csv_data(fmu_check_data_t* cdata, double time);
+
+
+#endif
